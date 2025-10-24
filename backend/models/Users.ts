a1929408ac6db9db524e31fa0cd1ee9608a1e70c
HBLUCF <an008721@ucf.edu>
2025-10-23 20:13:05 -0400

    Update Users.ts

--- backend/models/Users.ts
+++ backend/models/Users.ts
@@ -1,32 +1,48 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
+
+export interface IUser extends Document {
+  uid: number;
+  name: string;
+  email: string;
+  password: string;
+  problems_solved: number;
+}
 
 const userSchema = new mongoose.Schema({
   uid: { 
     type: Number, 
     required: true, 
-    unique: true },
+    unique: true 
+  },
     
   name: { 
     type: String, 
     required: true,
-    maxlength:255},
+    maxlength: 255
+  },
 
   email: { 
     type: String, 
     required: true,
     maxlength: 255, 
-    unique: true },
+    unique: true,
+    lowercase: true,  // acts as a toLower
+    trim: true        // clears the whitespace
+  },
 
   password: { 
     type: String, 
     required: true, 
-    minlength: 8, 
-    maxlength:255},
+    minlength: 8,     
+    maxlength: 255    
+  },
 
   problems_solved: { 
     type: Number, 
-    default: 0 }
+    default: 0 
+  }
+}, 
+
+export default mongoose.model<IUser>("User", userSchema);
 
 });
-
-export default mongoose.model("User", userSchema);
