895b72d25015c89ef1ef53a2bd2ac217def811ef
Laila <laila@ucf.edu>
2025-12-02 22:53:57 -0500

    fixing errors

--- mobile/ios/Runner/GeneratedPluginRegistrant.m
+++ mobile/ios/Runner/GeneratedPluginRegistrant.m
@@ -18,11 +18,18 @@
 @import shared_preferences_foundation;
 #endif
 
+#if __has_include(<webview_flutter_wkwebview/WebViewFlutterPlugin.h>)
+#import <webview_flutter_wkwebview/WebViewFlutterPlugin.h>
+#else
+@import webview_flutter_wkwebview;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [PathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"PathProviderPlugin"]];
   [SharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"SharedPreferencesPlugin"]];
+  [WebViewFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"WebViewFlutterPlugin"]];
 }
 
 @end
